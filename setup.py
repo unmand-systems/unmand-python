10ab3c0e0cee6d4f254d7dfc0c523bcd8d1012de
Marcu <marcu.knoesen@unmand.com>
2024-08-30 14:47:21 +1000

    Require python 3.11

--- setup.py
+++ setup.py
@@ -27,5 +27,5 @@ setuptools.setup(name='unmand',
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent"
         ],
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     zip_safe=False)
