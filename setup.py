b8727b6075cdb5232a3f5f9911a2bcf960e7810d
Joscar <joscar.needham@unmand.com>
2025-03-24 15:03:38 +1100

    add position option to exfil poll

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
         return f.read()
 
 setuptools.setup(name='unmand',
-    version='2.1.1',
+    version='2.1.2',
     description='Helper library for consuming the Unmand API',
     url='https://github.com/unmand-systems/unmand-python',
     author='Josiah Khor',
